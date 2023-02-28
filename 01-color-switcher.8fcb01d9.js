f62a4e7f6b81590e63f9092d3d363c5b08b6385d
eNgine9r <92324923+eNgine9r@users.noreply.github.com>
2023-02-28 19:46:10 +0000

    Deploying to gh-pages from @ eNgine9r/goit-js-hw-09@c773e6825c7135e79ca9d0dba82aa12b0f6af82f 🚀

---
+++ 01-color-switcher.8fcb01d9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,spanText.textContent=document.body.style.backgroundColor}));
+//# sourceMappingURL=01-color-switcher.8fcb01d9.js.map
