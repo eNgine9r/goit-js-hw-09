c773e6825c7135e79ca9d0dba82aa12b0f6af82f
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-02-28 21:45:44 +0200

    update task 1 / in progress

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onInputChange);
+
+function onInputChange () {
+    document.body.style.backgroundColor = getRandomHexColor();
+    spanText.textContent =  document.body.style.backgroundColor;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
