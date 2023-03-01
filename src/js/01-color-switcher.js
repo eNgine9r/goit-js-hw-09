4f45b34d1016e97831490ecb41bd13e649d3c52f
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-03-01 21:13:47 +0200

    Update task 1 | in progres

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,48 @@
+const DELAY_COLOR_SWITCHER = 1000;
+let timeoutId = null;
+let isActive = false;
 const refs = {
   btnStart: document.querySelector('[data-start]'),
   btnStop: document.querySelector('[data-stop]'),
 };
 
 
-refs.btnStart.addEventListener('click', onInputChange);
-refs.btnStop.addEventListener('click', stop);
+refs.btnStart.addEventListener('click', startColorSwitcher);
+refs.btnStop.addEventListener('click', stopColorSwitcher);
 
-function onInputChange () {
-    document.body.style.backgroundColor = getRandomHexColor();
-    
-    setInterval(() => {
-      onInputChange();
-      // console.log('hello')
-    }, 1000);
+function stopColorSwitcher (){
+  clearTimeout(timeoutId);
+}
+
+function startColorSwitcher() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  
+  if (isActive) {
+      return;
+    }
+    isActive = true;
+
+  timeoutId = setTimeout(() => {
+    startColorSwitcher();
+  }, DELAY_COLOR_SWITCHER);
 };
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// const switcher = {
+//   timeoutId: null,
+//   isActive: false,
+//   start() {
+//     if (isActive) {
+//       return;
+//     }
+    
+//     timeoutId = setTimeout(() => {
+//       document.body.style.backgroundColor = getRandomHexColor();
+//       startColorSwitcher();
+//     }, DELAY_COLOR_SWITCHER);
 
-
-
-function stop (){
-  clearInterval(intervalId);
-}
-
+//   }
+// }
